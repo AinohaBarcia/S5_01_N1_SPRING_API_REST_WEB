b87d3eb4fe9c74e2551a3e231fca64b2e1474fe3
ainohabarcia <ainoha.barcia@gmail.com>
2024-02-13 12:49:56 +0100

    Modoficada las exception de Security a CucursalException

--- src/main/java/cat/itacademy/barcelonactiva/barcia/ainoha/s05/t01/n01/model/services/ISucursalService.java
+++ src/main/java/cat/itacademy/barcelonactiva/barcia/ainoha/s05/t01/n01/model/services/ISucursalService.java
@@ -1,5 +1,6 @@
 package cat.itacademy.barcelonactiva.barcia.ainoha.s05.t01.n01.model.services;
 
+import cat.itacademy.barcelonactiva.barcia.ainoha.s05.t01.n01.SucursalException;
 import cat.itacademy.barcelonactiva.barcia.ainoha.s05.t01.n01.model.dto.SucursalDTO;
 
 import java.util.List;
@@ -10,9 +11,9 @@ public interface ISucursalService {
 
     SucursalDTO updateSucursal (SucursalDTO sucursalDTO);
 
-    SucursalDTO deleteSucursalById(Long pk_SucursalID)throws SecurityException;
+    SucursalDTO deleteSucursalById(Long pk_SucursalID)throws SucursalException;
 
-    SucursalDTO getSucursalById (Long pk_SucursalID)throws SecurityException;
+    SucursalDTO getSucursalById (Long pk_SucursalID)throws SucursalException;
 
     List<SucursalDTO> getAllSucurals();
 
